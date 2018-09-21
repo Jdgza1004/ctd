11cd45a0015491c319153f097cafcf0dd83d2833
Jennifer G <jennifer.garza2691@gmail.com>
2018-09-21 22:28:54 +0000

    redid as best I could with day one HW

--- week_one/reading_assignments/ruby_basics.rb
+++ week_one/reading_assignments/ruby_basics.rb
@@ -2,21 +2,31 @@
 # there is always more than one way to code a response 😄
 # please use your knowledge of variable assignments in each response
 # when you are done, share your code with your instructor, push it to Github, or submit a pull request
-
+=begin
+output means result of a equation or someway of getting something specific out of I don't fully get it but we SHALL see.
+I feel like wow this second day of instuction. I feel like I need another walk though to fully understand. ah If it was EASY
+well lets get to work! cheers Jenny
+=end
 
 # output an integer
-
-
-
+puts 5
+puts 8*4
+print '36'
 # output the number of seconds in a week
-
+puts 7*24*60*60
 
 # output a float
-
-
+puts 6.38
+#not sure what this part means, is it like decimal answers?#
 
 # output a string
-
-
-
+puts "I am a Rockstar"
+print "I am a rockstar"
+print "we are rocksars"
 # output concatination of strings
+puts "My favorite Idol singer is RAVI"
+puts name = ravi
+puts "my favorite idol is #{name}"
+puts "go boy go " *3
+puts "I like " + "Legend of Zelda"
+puts "skyward sword is "+"the best"
